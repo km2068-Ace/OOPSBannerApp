07b8e7293c51a182b5d1a9d7e071c9b188e9d746
km2068-Ace <km2068@srmist.edu.in>
2026-02-16 09:28:54 +0530

    UC2 OOPS Pattern

---
+++ UC2.java
@@ -0,0 +1,11 @@
+class OOPSBannerApp {
+	public static void main(String[] args){
+System.out.println("   ***   "+""+"   ***   "+""+"   *****    "+""+"    ***** ");
+System.out.println(" **   ** "+""+" **   ** "+""+"   **   **  "+""+"  **      ");
+System.out.println("**     **"+""+"**     **"+""+"   **    ** "+""+" **        ");
+System.out.println("**     **"+""+"**     **"+""+"   **   **  "+""+"  *****   ");
+System.out.println("**     **"+""+"**     **"+""+"   *****    "+""+"      **  ");
+System.out.println(" **   ** "+""+" **   ** "+""+"   **       "+""+" **   **  ");
+System.out.println("   ***   "+""+"   ***   "+""+"   **       "+""+"  *****   ");
+}
+}
