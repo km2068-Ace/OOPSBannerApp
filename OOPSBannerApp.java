ca4183fac978c501ac4621907abddf805a84fe7b
km2068-Ace <km2068@srmist.edu.in>
2026-02-17 08:35:11 +0530

    UC3 OOPS Pattern

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,10 +1,11 @@
 public class OOPSBannerApp {
 	public static void main(String[] args){
+	
 		System.out.println("OOPS");
-	
-	
-	
-	
-	
-	
+	}
 }
+	
+	
+	
+	
+	
---
+++ UC3.java
@@ -0,0 +1,12 @@
+class OOPSBannerApp {
+	public static void main(String[] args){
+		String d = "  ";
+System.out.println(String.join(d,"   ***   ","   ***   "," *****    ","    ***** "));
+System.out.println(String.join(d," **   ** "," **   ** "," **   **  ","  **      "));
+System.out.println(String.join(d,"**     **","**     **"," **    ** "," **        "));
+System.out.println(String.join(d,"**     **","**     **"," **   **  ","  *****   "));
+System.out.println(String.join(d,"**     **","**     **"," *****    ","      **  "));
+System.out.println(String.join(d," **   ** "," **   ** "," **       "," **   **  "));
+System.out.println(String.join(d,"   ***   ","   ***   "," **       ","  *****   "));
+}
+}
